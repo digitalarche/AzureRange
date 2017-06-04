cab343318a3109b9d226c0b31f65c1866b9f967d
Olivier Martin <omartin@live.ca>
2017-06-04 00:34:44 -0400

    Added controllers, required jscript and HTML for IP search tool.

---
+++ AzureRange.Website/Controllers/FindPrefixHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureRange.Website.Controllers
+{
+    public class FindPrefixHelper
+    {
+        internal static IPPrefix FindPrefix(IPPrefix p_inputPrefix)
+        {
+            var containingPrefix = new IPPrefix();
+            // Load the XML file into ranges
+            List<IPPrefix> ipPrefixes = Downloader.Download();
+            containingPrefix = Generator.GetContainingPrefix(p_inputPrefix, ipPrefixes);
+
+            return containingPrefix;
+        }
+    }
+}
